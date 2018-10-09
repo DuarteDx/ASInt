39af40d8372933e2a89778b91d1c0c3b7c1ef5c1
Duarte Dias <DuartexDias@gmail.com>
2018-10-09 10:31:44 +0100

    started working on distributed version

--- Labs/Lab3/Duarte/book.py
+++
@@ -1,7 +0,0 @@
-class book:
-
-    def __init__(self, author, title, publicationYear, id):
-        self.author = author
-        self.title = title
-        self.publicationYear = publicationYear
-        self.id = id
---
+++ Labs/Lab3/distributedVersion/server/book.py
@@ -0,0 +1,7 @@
+class book:
+
+    def __init__(self, author, title, publicationYear, id):
+        self.author = author
+        self.title = title
+        self.publicationYear = publicationYear
+        self.id = id
