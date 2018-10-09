b55a8d48606e3a4aecbad0b5ca4187a7641a8754
Lourenço Vaz Pato <lourencopatto@gmail.com>
2018-10-09 10:26:34 +0100

    dividi as classes em ficheiros

--- Labs/Lab3/Duarte/centralizedAPP.py
+++ Labs/Lab3/Duarte/centralizedAPP.py
@@ -1,110 +1,20 @@
+import book
+import bookDB
+import dbUI
 import pickle
-
-class book:
-
-    def __init__(self, author, title, publicationYear, id):
-        self.author = author
-        self.title = title
-        self.publicationYear = publicationYear
-        self.id = id
-    
-class bookDB:
-
-    def __init__(self):
-        self.currentId = 0
-        self.books = []
-
-    def insertBook(self, author, title, publicationYear):
-        """Inserts book into database"""
-        newBook = book(author, title, publicationYear, self.currentId)
-        self.books.append(newBook.__dict__)
-        self.currentId += 1
-        f = open("bookDatabase.pickle", "wb")
-        pickle.dump(self.__dict__, f)
-        f.close()
-
-    def showBook(self, id):
-        """Shows book information given an id"""
-        id = int(id)
-        if id < len(self.books):
-            print("Author: " + self.books[id]['author'])
-            print("Title: " + self.books[id]['title'])
-            print("Publication year: " + self.books[id]['publicationYear'])
-        else:
-            print("Ups, looks like that book isn't in the database")
-
-    def listAllAuthors(self):
-        """Displays all authors in database"""
-        for book in self.books:
-            print(book['author'])
-
-    def listBooksFromAuthor(self, author):
-        """Displays books from a given author"""
-        booksByAuthor = []
-        for book in self.books:
-            if book['author'] == author:
-                booksByAuthor.append(book['title'])
-        if len(booksByAuthor) == 0:
-            print("Looks like there are no books from that author")
-        else:
-            print(booksByAuthor)
-
-    def listBooksFromYear(self, year):
-        """Displays books published in a given year"""
-        booksByYear = []
-        for book in self.books:
-            if book['publicationYear'] == year:
-                booksByYear.append(book['title'])
-        if len(booksByYear) == 0:
-            print("Looks like there are no books from that year yet")
-        else:
-            print(booksByYear)
-
-class dbUI:
-
-    def __init__(self, db):
-        self.bookDB = db
-
-    def readInput(self):
-
-        action = input("Action (NEW, SHOW, AUTHORS, SEARCH_AUTH, SEARCH_YEAR, EXIT): ")
-
-        if action == "NEW":
-            author = input("Author name: ")
-            title = input("Book title: ")
-            publicationYear = input("Publication year: ")
-            bookDB.insertBook(author, title, publicationYear)
-
-        elif action == "SHOW":
-            id = input("Book id: ")
-            bookDB.showBook(id)
-
-        elif action == "AUTHORS":
-            bookDB.listAllAuthors()
-
-        elif action == "SEARCH_AUTH":
-            author = input("Author name: ")
-            bookDB.listBooksFromAuthor(author)
-
-        elif action == "SEARCH_YEAR":
-            year = input("Year: ")
-            bookDB.listBooksFromYear(year)
-
-        elif action == "EXIT":
-            return 0
 
 try:
     f = open("bookDatabase.pickle", "rb")
     oldData = pickle.load(f)
-    bookDB = bookDB()
+    bookDB = bookDB.bookDB()
     bookDB.books = oldData['books']
     bookDB.currentId = oldData['currentId']
     print(bookDB)
     f.close()
 except:
-    bookDB = bookDB()
+    bookDB = bookDB.bookDB()
 
-dbUI = dbUI(bookDB)
+dbUI = dbUI.dbUI(bookDB)
 
 while True:
     if dbUI.readInput() == 0:
