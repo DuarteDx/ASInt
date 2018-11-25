befe756c315c8784c2945c9fcbaf13bc08f90431
Duarte Dias <DuartexDias@gmail.com>
2018-11-25 01:49:07 +0000

    Server endpoints mapped out

--- Project/server/server.py
+++ Project/server/server.py
@@ -3,6 +3,9 @@ from flask import redirect
 from flask import request
 from flask import jsonify
 
+#Functions from functions.py
+import functions
+
 server = Flask(__name__)
 
 ###################
@@ -32,3 +35,27 @@ def getPeopleInRange():
 @app.route('/addBuildingToDB')
 def addBuildingToDB():
     #Get a building information from an admin and add the building to the database
+
+@app.route('/getLoggedInUsers')
+def getLoggedInUsers():
+    #Send a list logged in users to admin
+
+@app.route('/getPeopleByBuilding')
+def getPeopleByBuilding():
+    #Send a list of users in specific building
+
+@app.route('/getUserHistory')
+def getUserHistory():
+    #Send a list of exchanged messages and movements by users
+
+@app.route('/getMessagesByBuilding')
+def getMessagesByBuilding():
+    #Send a list of messages all exchanged in a specific building
+
+###############
+# BOT ENDPOINTS
+###############
+
+@app.route('/broadcastBotMessage')
+def broadcastBotMessage():
+    #Get a message from a bot and broadcast it to users inside the building associated to the bot
