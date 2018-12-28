a7d41a9bba06a44fe284df9ee13f812e122ae1ac
Duarte Dias <DuartexDias@gmail.com>
2018-12-28 18:08:36 +0000

    admin sends single building to server

--- Project/admin/ajaxFunctions.py
+++ Project/admin/ajaxFunctions.py
@@ -1,13 +1,18 @@
 import requests
+import json
 
 serverURL = 'http://127.0.0.1:5000'
 
 def makePostRequest(url, data):
-    response = requests.post(url, data)
-    return response
+    headers = {'Content-Type': 'application/json'}
+    S = requests.session()
+    r = S.post(url, headers=headers, json=data)
+    print(r.status_code, r.reason)
+    return r
 
 def addBuilding(buildingID, buildingName, latitude, longitude):
     #Make ajax call to server
     url = serverURL + '/addBuildingToDB'
     data = {'buildingID':buildingID, 'buildingName':buildingName, 'latitude':latitude, 'longitude':longitude}
+    print('Sending data: ' + str(data))
     makePostRequest(url, data)
