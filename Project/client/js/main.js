37197806c6ad904c093809d982f4e040a36c120b
Duarte Dias <DuartexDias@gmail.com>
2018-12-29 20:57:25 +0000

    client fully working

--- Project/client/js/main.js
+++ Project/client/js/main.js
@@ -1,6 +1,26 @@
 var serverURL = "http://127.0.0.1:5000";
 const dummyID = 81356;
 
+var userName;
+var userId;
+
+var authCode = getAuthCode();
+    getTokens(authCode).then(data => {
+        var accessToken = data['access_token'];
+        var refreshToken = data['refresh_token'];
+        console.log(accessToken);
+        console.log(refreshToken);
+        getUserInfo(accessToken, refreshToken).then(userData => {
+            userName = userData['name'];
+            userName = userName.split(' ');
+            userName = userName[0] + ' ' + userName[userName.length-1];
+            userId = userData['username'];
+            console.log('Username: ' + userName);
+            console.log('User id: ' + userId);
+            addUserToDB(userId, userName);
+        });
+    });
+
 var sendLocation = new Vue({
   el: '#sendLocation',
   data: {
@@ -8,7 +28,8 @@ var sendLocation = new Vue({
     locationSentLast: 'nothing sent'
   },
   created() {
-    //this.interval = setInterval(this.handleSubmit, 2000)
+    setTimeout(() => this.userName = userName, 2500);
+    setTimeout(() => this.userId = userId, 2500);
   },
   methods: {
     handleSubmit() {
@@ -37,22 +58,8 @@ var userInfo = new Vue({
     userName: ''
   },
   created() {
-    var authCode = getAuthCode();
-        getTokens(authCode).then(data => {
-            var accessToken = data['access_token'];
-            var refreshToken = data['refresh_token'];
-            console.log(accessToken);
-            console.log(refreshToken);
-            getUserInfo(accessToken, refreshToken).then(userData => {
-                this.userName = userData['name'];
-                this.userName = this.userName.split(' ');
-                this.userName = this.userName[0] + ' ' + this.userName[this.userName.length-1];
-                this.userId = userData['username'];
-                console.log('Username: ' + this.userName);
-                console.log('User id: ' + this.userId);
-                addUserToDB(this.userId, this.userName);
-            });
-        });
+    setTimeout(() => this.userName = userName, 2500);
+    setTimeout(() => this.userId = userId, 2500);
   }
 })
 
@@ -61,10 +68,14 @@ var sendRange = new Vue({
   data: {
     range: '',
   },
+  created() {
+    setTimeout(() => this.userName = userName, 2500);
+    setTimeout(() => this.userId = userId, 2500);
+  },
   methods: {
     handleSubmit() {
       url = serverURL + '/defineRange';
-      sendPostRequest(url, {'user':dummyID, 'range':this.range});
+      sendPostRequest(url, {'user':userId, 'range':this.range});
       console.log('[C]Range sent: ' + this.range);
     }
   }
@@ -75,10 +86,14 @@ var sendMessage = new Vue({
   data: {
     userMessage: '',
   },
+  created() {
+    setTimeout(() => this.userName = userName, 2500);
+    setTimeout(() => this.userId = userId, 2500);
+  },
   methods: {
     handleSubmit() {
       url = serverURL + '/broadcastClientMessage';
-      sendPostRequest(url, {'user':dummyID,'message':this.userMessage});
+      sendPostRequest(url, {'user':userId,'message':this.userMessage});
       console.log('[C]Message sent: ' + this.userMessage);
     }
   }
@@ -90,14 +105,16 @@ var getNearbyUsers = new Vue({
     nearbyUsersList: [],
   },
   created() {
-    //this.interval = setInterval(this.getListOfNearbyUsers, 2000)
+    setTimeout(() => this.userName = userName, 2500);
+    setTimeout(() => this.userId = userId, 2500);
+    this.interval = setInterval(this.getListOfNearbyUsers, 2000)
   },
   methods: {
     getListOfNearbyUsers() {
       //ToDo: Ajax code to get list of users
       url = serverURL + '/getPeopleInRange';
-      sendPostRequest(url, {'user':dummyID}, this.nearbyUsersList).then(data => this.nearbyUsersList = data);
-      console.log('[C]Requested nearbyUsersList for ' + dummyID);
+      sendPostRequest(url, {'user':userId}, this.nearbyUsersList).then(data => this.nearbyUsersList = data);
+      console.log('[C]Requested nearbyUsersList for ' + userId);
       
       console.log('[C]get users button pressed');
       console.log(this.nearbyUsersList);
@@ -111,14 +128,16 @@ var getMessages = new Vue({
     messageList: [],
   },
   created() {
-    //this.interval = setInterval(this.getListOfMessages, 2000)
+    setTimeout(() => this.userName = userName, 2500);
+    setTimeout(() => this.userId = userId, 2500);
+    this.interval = setInterval(this.getListOfMessages, 2000)
   },
   methods: {
     getListOfMessages() {
       //ToDo: Ajax code to get list of messages
       url = serverURL + '/getUserMessages';
-      sendPostRequest(url, {'user':dummyID}).then(data => this.messageList = data);
-      console.log('[C]Requested list of messages for ' + dummyID);
+      sendPostRequest(url, {'user':userId}).then(data => this.messageList = data);
+      console.log('[C]Requested list of messages for ' + userId);
 
       console.log('[C]get messages button pressed');
       this.messageListDummy = [{from: 'Duarte', content: 'Oi, tudo bem?'},
